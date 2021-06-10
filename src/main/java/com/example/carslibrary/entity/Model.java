63c7c625c4b4a711b30919cfb0ccd5ed9949ca69
Daniel <danielloza1108@gmail.com>
2021-06-10 14:25:12 +0200

    remove @Data from Entity (complications with declarative query methods), @Data use only in DTOs

--- src/main/java/com/example/carslibrary/entity/Model.java
+++ src/main/java/com/example/carslibrary/entity/Model.java
@@ -8,7 +8,6 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "model", indexes = @Index(columnList = "name"))
-@Data
 public class Model {
 
     @Id
@@ -22,4 +21,44 @@ public class Model {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_make")
     private Make make;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear_from() {
+        return year_from;
+    }
+
+    public void setYear_from(int year_from) {
+        this.year_from = year_from;
+    }
+
+    public int getYear_to() {
+        return year_to;
+    }
+
+    public void setYear_to(int year_to) {
+        this.year_to = year_to;
+    }
+
+    public Make getMake() {
+        return make;
+    }
+
+    public void setMake(Make make) {
+        this.make = make;
+    }
 }
